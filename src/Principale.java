6d7130101d12789606113775e50ae48a4b7935e9
4b <4b@INF05>
2022-04-23 11:49:29 +0200

    versione 1

---
+++ src/Principale.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Danilo Cossa
+ * 
+ * @version 1.0
+ *
+ */
+
+public class Principale {
+	public static void main(String[] args) {
+		System.out.println("prova github 1");
+	}
+
+}
