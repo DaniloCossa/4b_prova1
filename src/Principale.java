ea66851604e1c5b458a91327bd4643f672feffb1
Danilo 2 <4b@INF05>
2022-04-23 12:01:39 +0200

    varsione 2

--- src/Principale.java
+++ src/Principale.java
@@ -1,14 +1,14 @@
 /**
  * 
- * @author Danilo Cossa
+ * @author Danilo
  * 
- * @version 1.00
+ * @version 2.00
  *
  */
 
 public class Principale {
 	public static void main(String[] args) {
-		System.out.println("prova github 1");
+		System.out.println("prova github 2");
 	}
 
 }
